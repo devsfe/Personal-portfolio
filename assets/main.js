be1b7aa251cbb744415d6a17c69a5a30487f69e5
Fernando Chaves <fernando.chaves16@hotmail.com>
2020-11-19 21:52:23 -0300

    19 11 finalizado

--- assets/main.js
+++ assets/main.js
@@ -2,35 +2,16 @@ const menuBtn = document.querySelector('.bars');
 const navbar = document.querySelector('.nav');
 var burger = document.querySelector('.bars__burger')
 var menuOpen = false;
-var navList = document.querySelector('.nav__list');
 var body = document.querySelector('body');
-var container = document.querySelector('.container');
-var logo = document.querySelector('.nav__logo');
+const dataAnime = document.querySelectorAll('[data-anime]');
+const animate =  'animate';
 
 //Eventos
-window.addEventListener('scroll', stickyNav);
 menuBtn.addEventListener('click', btnAnimation);
-
-
-//Sticky nav
-function stickyNav() {
-    navbar.classList.toggle('sticky', window.scrollY > 10);
-    if(window.scrollY > 10) {
-        navLogo.style.color = '#292929';
-        menuMobile.style.color = '#292929';
-
-        for(var i = 0; i < navLink.length; i++) {
-            navLink[i].style.color = '#292929';
-        }
-    } else {
-        navLogo.style.color = '';
-        menuMobile.style.color = '';
-        for(var i = 0; i < navLink.length; i++) {
-            navLink[i].style.color = '';
-        }
-    }
-};
-
+if(dataAnime.length) {
+    window.addEventListener('scroll', animeScroll);
+    console.log('teste');
+}
 
 
 //Animação menu bars
@@ -43,3 +24,18 @@ function btnAnimation(){
         menuOpen = false;
     }
 };
+
+//Animate on scroll
+function animeScroll() {
+    const topoDoSite = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    dataAnime.forEach(function(e){
+        if((topoDoSite) > e.offsetTop) {
+            e.classList.add(animate);
+        } else {
+            e.classList.remove(animate);
+        }
+    });
+}
+
+
+animeScroll();
